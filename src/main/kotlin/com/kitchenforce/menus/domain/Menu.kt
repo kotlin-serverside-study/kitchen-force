a23ddfc325ddfd4fc3316352cdfb6b2ccb587eca
Jinho Hyeon <neverlish@gmail.com>
2022-01-08 19:02:58 +0900

    Entity에 Table, EntityListeners 적용

--- src/main/kotlin/com/kitchenforce/menus/domain/Menu.kt
+++ src/main/kotlin/com/kitchenforce/menus/domain/Menu.kt
@@ -1,10 +1,13 @@
 package com.kitchenforce.menus.domain
 
 import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.util.*
 import javax.persistence.*
 
 @Entity
+@Table
+@EntityListeners(AuditingEntityListener::class)
 class Menu(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
